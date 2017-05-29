a47a7bb02fc82cf7c99a42fd426a0a39e15e8e04
JRoger <sjr_vip@126.com>
2017-05-29 11:14:59 +0800

    add use sample.

--- src/RazorTemplates.Samples/Program.cs
+++ src/RazorTemplates.Samples/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using System;
 using RazorTemplates.Core;
 
 namespace RazorTemplates.Samples
@@ -9,6 +10,8 @@ namespace RazorTemplates.Samples
         {
             SimpleTemplate();
             TemplateWithCustomNamespaces();
+            TemplateWithFunction();
+            TemplateWithHelper();
 
             Console.ReadLine();
         }
@@ -29,6 +32,48 @@ namespace RazorTemplates.Samples
             Console.WriteLine(template.Render(new { Apples = 1 }));
             Console.WriteLine(template.Render(new { Apples = 2 }));
         }
+
+        public static void TemplateWithFunction()
+        {
+            var template = Template
+                .WithBaseType<TemplateBase>()
+                .AddNamespace("RazorTemplates.Samples")
+                .Compile(@"
+                    @functions
+                    {
+                        public string GetSomeString()
+                        {
+                            var str = string.Empty;
+                            var events = (List<EventModel>)Model.Events;
+                            for(var i = 0; i < events.Count; i++)
+                            {
+                                str += events[i].Name;
+                            }
+
+                            return str;
+                        }
+                    }
+                    
+@GetSomeString()
+                ");
+
+            Console.WriteLine(template.Render(new { Events = new List<EventModel> { new EventModel { Name = "apple - " }, new EventModel { Name = "apples" } } }));
+        }
+
+        public static void TemplateWithHelper()
+        {
+            var template = Template
+                .WithBaseType<TemplateBase>()
+                .AddNamespace("RazorTemplates.Samples")
+                .Compile(@"
+                    @{
+                        Func<string, string> func = str => { return str + 123.ToString(); };
+                    }
+@func(Model.Name)
+                ");
+
+            Console.WriteLine(template.Render(new { Name = "JRoger - " }));
+        }
     }
 
     public static class Plural
@@ -38,5 +83,10 @@ namespace RazorTemplates.Samples
             var form = value == 1 ? 0 : 1;
             return forms[form];
         }
+    }
+
+    public class EventModel
+    {
+        public string Name { get; set; }
     }
 }
