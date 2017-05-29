3ef683746b7004b7f03a75c6325da6267c818946
JRoger <sjr_vip@126.com>
2017-05-29 09:51:43 +0800

    add comment.

--- src/RazorTemplates.Core/TemplateCompilationLanguage.cs
+++ src/RazorTemplates.Core/TemplateCompilationLanguage.cs
@@ -1,8 +1,18 @@
 ﻿namespace RazorTemplates.Core
 {
+    /// <summary>
+    /// 表示模板使用的编译语言的枚举值。
+    /// </summary>
     public enum TemplateCompilationLanguage
     {
+        /// <summary>
+        /// CSharp
+        /// </summary>
         CSharp,
+
+        /// <summary>
+        /// VisualBasic
+        /// </summary>
         VisualBasic
     }
 }
